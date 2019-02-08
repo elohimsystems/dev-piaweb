88d5f0313685a0b6fef2f6d1b3f9a2b80e08c5c1
Freddy Garcia (Casa) <fgarcia@sistemapia.com>
2019-02-08 08:53:43 -0400

    continuamos con mejoras de patrocinantes

--- src/FraterSoft/PiaWebBundle/Controller/PatrocinanteController.php
+++ src/FraterSoft/PiaWebBundle/Controller/PatrocinanteController.php
@@ -6,186 +6,39 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DomCrawler\Crawler;
-use FraterSoft\PiaWebBundle\Entity\Numeracion;
-use FraterSoft\PiaWebBundle\Form\NumeracionType;
+use FraterSoft\PiaWebBundle\Entity\Patrocinante;
+use FraterSoft\PiaWebBundle\Form\PatrocinanteType;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityRepository;
 
 /**
  * Competidor controller.
  *
  */
-class NumeracionController extends commonPIAClass {
-    
-    private $numerados;
-    private $inicial; 
-    private $final;
-
+class PatrocinanteController extends commonPIAClass {
+        
     /**
-     * Lists all Competidor entities.
-     *
-     */
-    public function asignarAction($idevento) {
-        $respuesta=$this->enumerar($idevento);
-        if($respuesta>0){
-            return $this->render('FraterSoftPiaWebBundle:Numeracion:numerar.html.twig', array(
-                        'numerados' => $this->numerados,
-                        'inicial' => $this->inicial,
-                        'final' => $this->final
-            ));
-        }
-        else{
-            $request = $this->getRequest();
-            $referer = $request->headers->get('referer');   
-            if($respuesta==-1)
-                return $this->render('FraterSoftPiaWebBundle:Default:mensaje.html.twig', array(
-                            'url' => $referer,
-                            'texto' => 'No se ha configurado numeracion para este evento',
-                ));      
-            if($respuesta==-2)
-                return $this->render('FraterSoftPiaWebBundle:Default:mensaje.html.twig', array(
-                            'url' => null,
-                            'texto' => 'No existen numeros externos disponibles',
-                ));                
-        }
-    }  
-    
-    public function resetearAction($idevento) {
-        $em = $this->getDoctrine()->getManager();
-        $em->getRepository('FraterSoftPiaWebBundle:Inscrito')->resetearNumeracion($idevento);
-        $numeracion=$em->getRepository('FraterSoftPiaWebBundle:Numeracion')->findOneBy(array('idevento'=>$idevento));
-        if($numeracion){
-            $em->getRepository('FraterSoftPiaWebBundle:Numeracionexterna')->resetear($numeracion->getId());
-            $numeracion->setSiguiente($numeracion->getInicio());
-            $numeracionatributos=$em->getRepository('FraterSoftPiaWebBundle:Numeracionatributo')->findBy(array('idnumeracion'=>$numeracion->getId()));
-            foreach ($numeracionatributos as $numeracionatributo){
-                $numeracionatributo->setSiguiente($numeracionatributo->getInicio());
-            }
-            $em->flush();     
-        }
-        $request = $this->getRequest();
-        $referer = $request->headers->get('referer');
-        return $this->render('FraterSoftPiaWebBundle:Default:mensaje.html.twig', array(
-                    'url' => $referer,
-                    'texto' => 'Reseteada toda la numeracion del evento',
-        ));          
-    }
-    
-    public function listanumeracionAction($idevento,$email) {
-        $em = $this->getDoctrine()->getManager();
-
-        $inscritos = $em->getRepository('FraterSoftPiaWebBundle:Inscrito')
-                ->listarConciliadas($idevento);
-
-        $evento = $em->getRepository('FraterSoftPiaWebBundle:Evento')->find($idevento);     
-
-        return $this->render('FraterSoftPiaWebBundle:Numeracion:lista_numeracion.html.twig', array(
-                    'inscritos' => $inscritos,
-                    'idevento' => $idevento,
-                    'email' => $email,
-        ));
-    }      
-    
-    public function enumerar($idevento){
-        $em = $this->getDoctrine()->getManager();
-        $inscrito=null;
-        $contador=1;
-        $numeracion = $em->getRepository('FraterSoftPiaWebBundle:Numeracion')->findOneBy(array('idevento' => $idevento));
-        if(!$numeracion) //si no hay numeracion configurada
-            return(-1);
-        $numeracionatributos = $em->getRepository('FraterSoftPiaWebBundle:Numeracionatributo')->findBy(array('idnumeracion' => $numeracion->getId()));
-        if(!$numeracionatributos){ //Si no hay atributos configurados se usa configuracion por evento
-            $this->numerados=array(0);
-            $this->inicial=array(0);
-            $this->final=array(0);
-            $inscritos = $em->getRepository('FraterSoftPiaWebBundle:Inscrito')->listaParaNumerar(
-                    $idevento,null,$numeracion->getOrdenarpor()
-            );
-            foreach ($inscritos as $inscrito){
-                $inscripcion=$em->getRepository('FraterSoftPiaWebBundle:Inscrito')->find($inscrito);
-                if($numeracion->getSecuencial()){
-                    $this->numerados[0]=$contador++;
-                    if($numeracion->getSiguiente()==$numeracion->getInicio())
-                        $this->inicial[0]=$numeracion->getSiguiente();
-                    $inscripcion->setNumero($numeracion->getSiguiente());
-                    $this->final[0]=$numeracion->getSiguiente();
-                    $numeracion->setSiguiente($numeracion->getSiguiente()+1);
-                }
-                else{
-                    $numeracionexterna = $em->getRepository('FraterSoftPiaWebBundle:Numeracionexterna')->findOneBy(
-                            array('idnumeracion'=>$numeracion->getId(),'asignado'=>false),
-                            array('numero'=>'ASC'),
-                            1 //parametro limit (devuelve 1 registro de los seleccionados)
-                    );
-                    if($numeracionexterna){ 
-                        $this->numerados[0]=$contador++;
-                        $inscripcion->setNumero($numeracionexterna->getNumero());
-                        $numeracionexterna->setAsignado(true);
-                    }
-                    else{
-                         return(commonPIAClass::NUMERACIONEXTERNA_NO_CONFIGURADA);
-                    }
-                    $em->flush();
-                }
-            }
-            $em->flush();
-        }
-        else{
-            $this->numerados=array();
-            $this->inicial=array();
-            $this->final=array();                
-            foreach ($numeracionatributos as $numeracionatributo){
-                switch (true){
-                    case $numeracion->getAtributo()=="competencia":
-                        $filtro="tmcompetencias.descripcion in (" . $numeracionatributo->getValoratributo() . ")";
-                        break;
-                    case $numeracion->getAtributo()=="categortia":
-                        $filtro="tmcategorias.descripcion in (" . $numeracionatributo->getValoratributo() . ")";
-                        break;                
-                    case $numeracion->getAtributo()=="precio":
-                        $filtro="tminscritos.precio in (" . $numeracionatributo->getValoratributo() . ")";
-                        break;        
-                    default:
-                        $filtro="tmcompetidores." . $numeracion->getAtributo() . " in (" . $numeracionatributo->getValoratributo() . ")";
-                }
-                $idinscritos = $em->getRepository('FraterSoftPiaWebBundle:Inscrito')->listaParaNumerar(
-                        $idevento,$filtro,$numeracion->getOrdenarpor()
-                );
-                $this->inicial[$numeracionatributo->getValoratributo()]=$numeracionatributo->getSiguiente(); 
-                $contador=1;
-                foreach ($idinscritos as $idinscrito){
-                    $this->numerados[$numeracionatributo->getValoratributo()]=$contador++;
-                    $inscripcion=$em->getRepository('FraterSoftPiaWebBundle:Inscrito')->find($idinscrito);
-                    $inscripcion->setNumero($numeracionatributo->getSiguiente());
-                    $this->final[$numeracionatributo->getValoratributo()]=$numeracionatributo->getSiguiente();
-                    $numeracionatributo->setSiguiente($numeracionatributo->getSiguiente()+1);
-                }
-                $em->flush();     
-            }
-        }
-        return($contador);
-    }
-    
-    /**
-     * Creates a new Numeracion entity.
+     * Creates a new Patrocinante entity.
      *
      */
     public function createAction(Request $request)
     {
-        $entity = new Numeracion();
+        $entity = new Patrocinante();
         $form = $this->createCreateForm($entity);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+            print_r("entre");
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('numeracion_gestion', array(
-                'idevento' => $entity->getIdevento()->getId(),
+            return $this->redirect($this->generateUrl('patrocinante_gestion', array(
+                'idorganizador' => $entity->getIdorganizador()->getId(),
                 'estado' => 2
             )));            
         }
@@ -193,65 +46,62 @@ class NumeracionController extends commonPIAClass {
         return $this->render('FraterSoftPiaWebBundle:Default:mensaje.html.twig', array(
                     'url' => null,
                     'texto' => json_encode($errors),
-                    'tema' => $entity->getIdevento()->getTema()
         ));                
 
     }
 
     /**
-    * Creates a form to create a Numeracion entity.
+    * Creates a form to create a Patrocinante entity.
     *
-    * @param Numeracion $entity The entity
+    * @param Patrocinante $entity The entity
     *
     * @return \Symfony\Component\Form\Form The form
     */
-    private function createCreateForm(Numeracion $entity)
+    private function createCreateForm(Patrocinante $entity)
     {
-        $form = $this->createForm(new NumeracionType(), $entity, array(
+        $form = $this->createForm(new PatrocinanteType(), $entity, array(
             'method' => 'POST',
         ));
         return $form;
     }
 
     /**
-     * Displays a form to create a new Numeracion entity.
+     * Displays a form to create a new Patrocinante entity.
      *
      */
-    public function gestionAction($idevento)
+    public function gestionAction($idorganizador)
     {
-        $entity = new Numeracion();
+        $entity = new Patrocinante();
         $form   = $this->createCreateForm($entity);
         
         $em = $this->getDoctrine()->getManager();
-        
-        //llena el Select con el evento y Oculta el control
-//        $form->add('idevento','entity',array(
-//                'class' => 'FraterSoftPiaWebBundle:Evento',
-//                'query_builder' => function (EntityRepository $er) use ( $idevento ) {
-//                    return $er->createQueryBuilder('e')
-//                            ->where('e.id=:idevento')
-//                            ->setParameter('idevento',$idevento);
-//                },
-//            ));
-        
 
-        $atributos = $em->getRepository('FraterSoftPiaWebBundle:EventoAtributos')->atributosOptions($idevento);
+        //llena el Select con el organizador y Oculta el control
+        $form->add('idorganizador','entity',array(
+                'class' => 'FraterSoftPiaWebBundle:Organizador',
+                //'attr'=>array('style'=>'display:none'),
+                //'label_attr'=>array('style'=>'display:none'),
+                'query_builder' => function (EntityRepository $er) use ( $idorganizador ) {
+                    return $er->createQueryBuilder('o')
+                            ->where('o.id=:idorganizador')
+                            ->setParameter('idorganizador',$idorganizador);
+                },
+            ));
         
-        return $this->render('FraterSoftPiaWebBundle:Numeracion:gestion.html.twig', array(
+        return $this->render('FraterSoftPiaWebBundle:Patrocinante:gestion.html.twig', array(
             'form'   => $form->createView(),
-            'idevento' => $idevento,
-            'atributos' => $atributos, 
-            'campos' => $this->getCampos($em,'Numeracion'),
+            'idorganizador' => $idorganizador,
+            'campos' => $this->getCampos($em,'Patrocinante'),
         ));
     }
 
-    public function listaAjaxAction($idevento){
+    public function listaAjaxAction($idorganizador){
         $encoders = array(new XmlEncoder(), new JsonEncoder());
         $normalizers = array(new GetSetMethodNormalizer());  
         $serializer = new Serializer($normalizers, $encoders);  
         
         $em = $this->getDoctrine()->getManager();
-        $entities = $em->getRepository('FraterSoftPiaWebBundle:Numeracion')->arrayListas($idevento);
+        $entities = $em->getRepository('FraterSoftPiaWebBundle:Patrocinante')->arrayListas($idorganizador);
 
         $jsonContent = $serializer->serialize(array(
             "recordsTotal"=> count($entities),
@@ -262,15 +112,15 @@ class NumeracionController extends commonPIAClass {
     }
 
     /**
-    * Creates a form to edit a Numeracion entity.
+    * Creates a form to edit a Patrocinante entity.
     *
-    * @param Numeracion $entity The entity
+    * @param Patrocinante $entity The entity
     *
     * @return \Symfony\Component\Form\Form The form
     */
-    private function createEditForm(Numeracion $entity)
+    private function createEditForm(Patrocinante $entity)
     {
-        $form = $this->createForm(new NumeracionType(), $entity, array(
+        $form = $this->createForm(new PatrocinanteType(), $entity, array(
             'method' => 'POST',
         ));
 
@@ -278,27 +128,28 @@ class NumeracionController extends commonPIAClass {
     }
     
     /**
-     * Edits an existing Numeracion entity.
+     * Edits an existing Patrocinante entity.
      *
      */
     public function updateAction(Request $request, $id)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('FraterSoftPiaWebBundle:Numeracion')->find($id);
+        $entity = $em->getRepository('FraterSoftPiaWebBundle:Patrocinante')->find($id);
 
         if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Numeracion entity.');
+            throw $this->createNotFoundException('Unable to find Patrocinante entity.');
         }
 
         $editForm = $this->createEditForm($entity);
+
         $editForm->handleRequest($request);
 
         if ($editForm->isValid()) {
             $em->flush();
 
-            return $this->redirect($this->generateUrl('numeracion_gestion', array(
-                'idevento' => $entity->getIdevento()->getId(),
+            return $this->redirect($this->generateUrl('patrocinante_gestion', array(
+                'idorganizador' => $entity->getIdorganizador()->getId(),
                 'estado' => 3
             )));            
         }
@@ -307,25 +158,24 @@ class NumeracionController extends commonPIAClass {
         return $this->render('FraterSoftPiaWebBundle:Default:mensaje.html.twig', array(
                     'url' => null,
                     'texto' => json_encode($errors),
-                    'tema' => $entity->getIdevento()->getTema()
         ));        
     }
     
     /**
-     * Deletes a Numeracion entity.
+     * Deletes a Patrocinante entity.
      *
      */
     public function deleteAction(Request $request, $id)
     {
         $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository('FraterSoftPiaWebBundle:Numeracion')->find($id);
+        $entity = $em->getRepository('FraterSoftPiaWebBundle:Patrocinante')->find($id);
         if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Numeracion entity.');
+            throw $this->createNotFoundException('Unable to find Patrocinante entity.');
         }
         $em->remove($entity);
         $em->flush();
-            return $this->redirect($this->generateUrl('numeracion_gestion', array(
-                'idevento' => $entity->getIdevento()->getId(),
+            return $this->redirect($this->generateUrl('patrocinante_gestion', array(
+                'idorganizador' => $entity->getIdorganizador()->getId(),
                 'estado' => 1
             )));            
     }
--- src/FraterSoft/PiaWebBundle/Form/PatrocinanteType.php
+++ src/FraterSoft/PiaWebBundle/Form/PatrocinanteType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class NumeracionType extends AbstractType
+class PatrocinanteType extends AbstractType
 {
         /**
      * @param FormBuilderInterface $builder
@@ -15,13 +15,10 @@ class NumeracionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('atributo')
-            ->add('automatica')
-            ->add('inicio')
-            ->add('fin')
-            ->add('siguiente')            
-            ->add('ordenarpor')            
-            ->add('secuencial')            
+            ->add('idorganizador')
+            ->add('logo')
+            ->add('nombre')
+            ->add('estatus')
         ;
     }
     
@@ -31,7 +28,7 @@ class NumeracionType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'FraterSoft\PiaWebBundle\Entity\Numeracion'
+            'data_class' => 'FraterSoft\PiaWebBundle\Entity\Patrocinante'
         ));
     }
 
@@ -40,6 +37,6 @@ class NumeracionType extends AbstractType
      */
     public function getName()
     {
-        return 'fratersoft_piawebbundle_numeracion';
+        return 'fratersoft_piawebbundle_patrocinante';
     }
 }
